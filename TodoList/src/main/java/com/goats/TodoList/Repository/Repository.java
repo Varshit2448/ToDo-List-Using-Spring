6069d2d818defaf9f38d7f2dd80ba0b2d54a2065
Varshit2448 <140231744+Varshit2448@users.noreply.github.com>
2025-07-29 08:14:18 +0530

    Delete TodoList directory

--- TodoList/src/main/java/com/goats/TodoList/Repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.goats.TodoList.Repository;
-
-import com.goats.TodoList.Models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository <Task,Integer> {
-
-}
--- TodoList/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO task VALUES (1,'Finish project', 0);
-INSERT INTO task VALUES (2,'Review PR', 1);
